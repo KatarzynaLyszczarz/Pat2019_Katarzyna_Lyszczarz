ac390a983b2a778dd5f4b1b27aa292eb952d9a64
Katarzyna Lyszczarz <kxlyszczarz@gmail.com>
2019-01-02 18:57:49 +0100

    Added application template

---
+++ app/src/main/java/com/example/katarzyna/katarzynalyszczarz/MainAcivity.kt
@@ -0,0 +1,12 @@
+package com.example.katarzyna.katarzynalyszczarz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
