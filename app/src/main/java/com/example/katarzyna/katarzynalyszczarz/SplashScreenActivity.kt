6fedd3788a8ad55cf4f806fd5d17a81a4b3959cd
Katarzyna Lyszczarz <kxlyszczarz@gmail.com>
2019-01-03 01:04:51 +0100

    Added splash screen

---
+++ app/src/main/java/com/example/katarzyna/katarzynalyszczarz/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.katarzyna.katarzynalyszczarz
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        val background = object : Thread(){
+            override fun run() {
+                try{
+                    Thread.sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+
+        }
+            background.start()
+    }
+}
