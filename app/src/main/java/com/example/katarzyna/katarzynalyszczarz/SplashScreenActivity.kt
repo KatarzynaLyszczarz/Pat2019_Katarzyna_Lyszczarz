c1fa91b28d5d70e2e93de321f9d128f23702e7db
Katarzyna Lyszczarz <kxlyszczarz@gmail.com>
2019-01-03 20:24:07 +0100

    Added onBackPressed method

--- app/src/main/java/com/example/katarzyna/katarzynalyszczarz/SplashScreenActivity.kt
+++ app/src/main/java/com/example/katarzyna/katarzynalyszczarz/SplashScreenActivity.kt
@@ -1,28 +1,28 @@
 package com.example.katarzyna.katarzynalyszczarz
-
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class SplashScreenActivity : AppCompatActivity() {
 
+    override fun onBackPressed() {
+        transitioner.interrupt() }
+
+    private val transitioner = object : Thread(){
+        override fun run() = try{
+            Thread.sleep(5000)
+            val intent = Intent(baseContext, MainActivity::class.java)
+            finish()
+            startActivity(intent)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        val background = object : Thread(){
-            override fun run() {
-                try{
-                    Thread.sleep(5000)
-
-                    val intent = Intent(baseContext, MainActivity::class.java)
-                    startActivity(intent)
-                } catch (e: Exception){
-                    e.printStackTrace()
-                }
-            }
-
-        }
-            background.start()
+        transitioner.start()
     }
 }
