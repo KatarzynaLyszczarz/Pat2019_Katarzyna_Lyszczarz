3cdc2c0a92b87efa6f3f5ae9f9d90a853718992b
Katarzyna Lyszczarz <kxlyszczarz@gmail.com>
2019-01-14 16:00:35 +0100

    Added Handler

--- app/src/main/java/com/example/katarzyna/katarzynalyszczarz/SplashScreenActivity.kt
+++ app/src/main/java/com/example/katarzyna/katarzynalyszczarz/SplashScreenActivity.kt
@@ -1,28 +1,30 @@
 package com.example.katarzyna.katarzynalyszczarz
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    override fun onBackPressed() {
-        transitioner.interrupt() }
+    private val transitionHandler = Handler()
 
-    private val transitioner = object : Thread(){
-        override fun run() = try{
-            Thread.sleep(5000)
+    private val transitioner = object : Thread() {
+        override fun run() = try {
             val intent = Intent(baseContext, MainActivity::class.java)
             finish()
             startActivity(intent)
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        transitionHandler.postDelayed(transitioner, 5000)
+    }
 
-        transitioner.start()
+    override fun onBackPressed() {
+        transitionHandler.removeCallbacksAndMessages(null)
     }
 }
